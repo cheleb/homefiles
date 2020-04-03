7c27515723a9f9021381f59ca6b92fc916fbbd1b
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2020-04-03 16:15:03 +0200

    Add gradle support (beta).
    Should know more about gradle cache layout.

--- bin/jarhardlinker.pl
+++ bin/jarhardlinker.pl
@@ -5,53 +5,122 @@ use strict;
 use Getopt::Long;
 use File::Next;
 use File::Slurp;
+use File::stat;
 
 my $nexus="/opt/nexus/sonatype-work/nexus3/blobs/default/content";
-my ($maven, $sbt, $debug);
+my ($gradle, $maven, $sbt, $debug, $dryRun);
 
 GetOptions ("nexus=s"  => \$nexus,   # String
             "maven"    => \$maven,   # string
             "sbt"      => \$sbt,     # string
-            "debug"    => \$debug)   # Numeric
+            "gradle"   => \$gradle,
+            "debug"    => \$debug,
+            "dry-run"  => \$dryRun
+            )   # Numeric
 or die("Error in command line arguments\n");
  
-my $nexusIter = File::Next::files({ 'file_filter' => sub { /\.properties$/ } }, $nexus );
-
-my $count = 0; 
-my %files =();
-while ( defined ( my $file = $nexusIter->() ) ) {
-    #print $file, "\n";
-    my %meta = read_file($file) =~ /^([^=]+)=(.*)$/mg;
-    #print $meta{'@BlobStore.content-type'}, "\n" unless $meta{'@BlobStore.content-type'} eq "application/java-archive";
-    next if $meta{'@BlobStore.content-type'} ne "application/java-archive";
-    $file =~ s/\.properties$/.bytes/;
-    $files{$meta{'@BlobStore.blob-name'}} = $file;
-    #print $meta{'@BlobStore.blob-name'}, "\n";
-    $count++;
-}
+my $mavenCache = $ENV{"HOME"} . "/.m2/repository/";
+my $sbtCache = $ENV{'HOME'} . '/Library/Caches/Coursier/v1/http/localhost%3A8081/repository/';
+my $gradleCache = $ENV{'HOME'} . '/.gradle/caches/modules-2/files-2.1/';
 
 
-linkSbtFile() if $sbt;
+my $nexusFiles = buildNexusIndex($nexus);
 
 
-sub linkSbtFile(){
-   print "Hard linking sbt\n";
-   my $sbtCache = $ENV{'HOME'} . '/Library/Caches/Coursier/v1/http/localhost%3A8081/repository/';
-   my $sbtIter = File::Next::files({ 'file_filter' => sub { /\.jar$/ } }, $sbtCache );
+hardLinkFiles("sbt", $sbtCache, sub {
+    my $relative = shift;
+    my ($repo, @paths) = split('/', $relative);
+    join('/', @paths);
+}) if $sbt;
+
+hardLinkFiles("mvn", $mavenCache) if $maven;
+hardLinkFiles("gradle", $gradleCache, sub {
+    my $relative = shift;
+    my ($groupId, $artifactId, $version, @others) = split('/', $relative);
+    $groupId =~ s!\.!/!g;
+    join('/', $groupId, $artifactId, $version, $artifactId.'-'.$version.'.jar');
+} ) if $gradle;
+
+
+sub hardLinkFiles(){
+   my $type = shift;
+   my $cachePath = shift;
+   my $resolver = shift
+    || sub {$_[0]};
+
+
+   my $stats = {};
+   debug("Hard linking $cachePath");
+   my $count=0;
+   my $sbtIter = File::Next::files({ 'file_filter' => sub { /\.jar$/ } }, $cachePath );
    while ( defined ( my $file = $sbtIter->() ) ) {
-       my $relative = substr($file, length($sbtCache));
-       my ($repo, @paths) = split('/', $relative);
-       my $path = join('/', @paths);
-       harlink($path, $file) if exists $files{$path};
-       print $path, "\n" unless exists $files{$path};
+       my $relative = substr($file, length($cachePath));
+       my $path = $resolver->($relative);
+       $count += harlink($path, $file, $type), $stats;
    }
-   print "\n";
+   printStats($type, $count, $cachePath, $stats);
+
 }
 
 sub harlink {
     my $path = shift;
     my $file = shift;
-    die $files{$path} unless -e $files{$path}; 
-    unlink $file;
-    link  $files{$path}, $file;
+    my $type = shift;
+    my $stats = shift;
+    if($dryRun){
+      debug(" 👎 $path") unless $nexusFiles->{$path};
+      debug(" 👍 $path") if $nexusFiles->{$path};
+    }elsif( -e $nexusFiles->{$path}){
+      my $st = stat($file);
+      return 0 if $st->nlink > 1; 
+      $stats->{"newly size red"} += $st->size;
+      $stats->{'newly hardlinked'}++;
+      unlink $file;
+      link  $nexusFiles->{$path}, $file;
+      1
+    }else{
+     $stats->{'unmanaged'}++;
+     debug("[-]  $path"); 
+     0
+    }
+}
+
+
+
+sub printStats {
+    my $type = shift;
+    my $count = shift;
+    my $cachePath = shift;
+    my $stats=shift;
+    print $type, ': ', $count, "\n";
+    my $sbtIter = File::Next::files({ 'file_filter' => sub { /\.jar$/ } }, $cachePath );
+    while ( defined ( my $file = $sbtIter->() ) ) {
+        my $st = stat($file);
+        $stats->{"size red"} += $st->size;
+        $stats->{'nlink '.$st->nlink}++;
+    }
+    foreach my $key  (sort keys %{$stats}) {
+        print "\t", $key, ': ', $stats->{$key}, "\n"
+    }
+
+}
+
+sub buildNexusIndex {
+  my $nexus = shift;
+  my $index = {};
+#  return $index if $dryRun;
+  debug("Build nexus install from:\n\t$nexus" );
+  my $nexusIter = File::Next::files({ 'file_filter' => sub { /\.properties$/ } }, $nexus );
+  while ( defined ( my $file = $nexusIter->() ) ) {
+    my %meta = read_file($file) =~ /^([^=]+)=(.*)$/mg;
+    next if $meta{'@BlobStore.content-type'} ne "application/java-archive";
+    $file =~ s/\.properties$/.bytes/;
+    $index->{$meta{'@BlobStore.blob-name'}} = $file;
+  }
+  $index
+}
+
+sub debug {
+    my $msg = shift;
+    warn $msg, , "\n" if($debug)
 }
