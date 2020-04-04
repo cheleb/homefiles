fa4ac5a007f2f3f00de5fb239b7fe1ced1fe111e
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2020-04-04 17:45:09 +0200

    Add unmanaged stats

--- bin/jarhardlinker.pl
+++ bin/jarhardlinker.pl
@@ -8,14 +8,15 @@ use File::Slurp;
 use File::stat;
 
 my $nexus="/opt/nexus/sonatype-work/nexus3/blobs/default/content";
-my ($gradle, $maven, $sbt, $debug, $dryRun);
+my ($gradle, $maven, $sbt, $debug, $dryRun, $unmanaged);
 
 GetOptions ("nexus=s"  => \$nexus,   # String
             "maven"    => \$maven,   # string
             "sbt"      => \$sbt,     # string
             "gradle"   => \$gradle,
             "debug"    => \$debug,
-            "dry-run"  => \$dryRun
+            "dry-run"  => \$dryRun,
+            "unmanaged" => \$unmanaged
             )   # Numeric
 or die("Error in command line arguments\n");
  
@@ -23,6 +24,8 @@ my $mavenCache = $ENV{"HOME"} . "/.m2/repository/";
 my $sbtCache = $ENV{'HOME'} . '/Library/Caches/Coursier/v1/http/localhost%3A8081/repository/';
 my $gradleCache = $ENV{'HOME'} . '/.gradle/caches/modules-2/files-2.1/';
 
+
+unmanaged() if $unmanaged;
 
 my $nexusFiles = buildNexusIndex($nexus);
 
@@ -36,9 +39,9 @@ hardLinkFiles("sbt", $sbtCache, sub {
 hardLinkFiles("mvn", $mavenCache) if $maven;
 hardLinkFiles("gradle", $gradleCache, sub {
     my $relative = shift;
-    my ($groupId, $artifactId, $version, @others) = split('/', $relative);
+    my ($groupId, $artifactId, $version, $sha, $filename) = split('/', $relative);
     $groupId =~ s!\.!/!g;
-    join('/', $groupId, $artifactId, $version, $artifactId.'-'.$version.'.jar');
+    join('/', $groupId, $artifactId, $version, $filename);
 } ) if $gradle;
 
 
@@ -69,10 +72,10 @@ sub harlink {
     my $stats = shift;
     if($dryRun){
       debug(" 👎 $path") unless $nexusFiles->{$path};
-      debug(" 👍 $path") if $nexusFiles->{$path};
+  #    debug(" 👍 $path") if $nexusFiles->{$path};
     }elsif( -e $nexusFiles->{$path}){
       my $st = stat($file);
-      return 0 if $st->nlink > 1; 
+#      return 0 if $st->nlink > 1; 
       $stats->{"newly size red"} += $st->size;
       $stats->{'newly hardlinked'}++;
       unlink $file;
@@ -96,8 +99,10 @@ sub printStats {
     my $sbtIter = File::Next::files({ 'file_filter' => sub { /\.jar$/ } }, $cachePath );
     while ( defined ( my $file = $sbtIter->() ) ) {
         my $st = stat($file);
+        next if $st->nlink == 1;
         $stats->{"size red"} += $st->size;
         $stats->{'nlink '.$st->nlink}++;
+        print $file, "\n" if $st->nlink == 4;
     }
     foreach my $key  (sort keys %{$stats}) {
         print "\t", $key, ': ', $stats->{$key}, "\n"
@@ -115,9 +120,46 @@ sub buildNexusIndex {
     my %meta = read_file($file) =~ /^([^=]+)=(.*)$/mg;
     next if $meta{'@BlobStore.content-type'} ne "application/java-archive";
     $file =~ s/\.properties$/.bytes/;
-    $index->{$meta{'@BlobStore.blob-name'}} = $file;
+    my $path = $meta{'@BlobStore.blob-name'};
+    my $uriEncodedPath = $path;
+    $uriEncodedPath =~ s/\+/%2B/g;
+    if($path ne $uriEncodedPath){
+      debug("encoded($path) |--> $uriEncodedPath");
+      $index->{$uriEncodedPath} = $file;
+    } else {
+      $index->{$path} = $file;
+    }
   }
   $index
+}
+
+sub unmanaged {
+  my $stats = {};
+  unmanagedInCache("sbt", $sbtCache, $stats) if $sbt;  
+  foreach my $key  (sort keys %{$stats}) {
+        print "\t", $key, ': ', $stats->{$key}, "\n"
+    }
+  exit
+}
+
+sub unmanagedInCache {
+  my $type = shift;
+  my $cache = shift;
+  my $stats = shift;
+
+  my $iter = jarIterator($cache);
+  while ( defined ( my $file = $iter->() ) ) {
+    my $st = stat($file);
+    next unless $st->nlink == 1;
+    print $file, "\n";
+    $stats->{"unmanaged in $type"}++
+  }
+}
+
+
+sub jarIterator {
+  my $cachePath = shift;
+  File::Next::files({ 'file_filter' => sub { /\.jar$/ } }, $cachePath );
 }
 
 sub debug {
