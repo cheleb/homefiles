54afe86b95d65a456307c13331fb0e6197b3bed5
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2020-04-05 23:04:19 +0200

    Much simpler.

--- bin/delorean.pl
+++ bin/delorean.pl
@@ -5,10 +5,11 @@ use strict;
 use Getopt::Long;
 use MetaBookMarks::Git;
 
+use Term::UI;
+
 my $from;
 my $to;
 my $debugLevel=0;
-my $sessionFile = "/tmp/delorean-" . getpwuid( $< );
 my $dryRun;
 GetOptions ("from=s"   => \$from,         # String
             "to=s"     => \$to,           # string
@@ -16,107 +17,84 @@ GetOptions ("from=s"   => \$from,         # String
             'dry-run'   => \$dryRun)      # String
 or die("Error in command line arguments\n");
 
-my $repo = MetaBookMarks::Git->repository();
+my $git = MetaBookMarks::Git->repository();
 
-my $continueSession = -e $sessionFile;
+my $branch = $git->branch(); 
 
-my $branch = $repo->branch(); 
-
-$to = $repo->headCommit();
+$to = $git->headCommit();
 
 dryRun() if $dryRun;
 
-die "\n\t 💀 Repository has changes!\n" if $repo->isDirty();
+die "\n\t 💀 Repository has changes!\n" if $git->isDirty();
 
-newSession() unless $continueSession;
+my @commits = newSession();
 
-if($repo->isDetachedHead()){
-  my $notDone;
-  do{
-    startOrContinueSession();
-    $notDone = <STDIN>;
-    chomp($notDone)
-  }while($notDone eq "")
-} else {
-  unlink $sessionFile;
-  die "\n\t 💥 Not HEAD\n\t($sessionFile deleted)\n";
-}
+my $index=0;
+do{
+  $index = nextCommit($index);
+  my $term = Term::ReadLine->new('brand');
+  print "\t";
+  my $next = $term->ask_yn(
+                            prompt => "Next",
+                            default => 'y',
+                    );
+  end() unless $next;
+  #$index = $i if( 0 <= $i && $i < $#commits);
+}while($index);
+
 
 sub newSession {
   die "\n\t 💥 Start commit must be provided with:\n\t\t --from COMMIT-HASH\n" unless $from;
-  my $branch = $repo->branch();
+  my $branch = $git->branch();
   print " 🚀 ", $branch, "\n";
   debug("from: $from");
   debug("to: $to");
-  debug("Session file $sessionFile");
-  system "git log --reverse --oneline --no-abbrev-commit $branch > $sessionFile";  
-  debug("Init history.");
-  system "git checkout $from" unless $continueSession;
-  debug("Starting...");
-  debug($repo->headCommit())
+  initCommits()
 }
 
-sub startOrContinueSession {
-    &debug("Continuing...") if $continueSession;
-    open my $history, "< $sessionFile" 
-      || die "Road is broken\n";
-    my $prev=<$history>;
-    my $current;
-    my $nextSha;
-    my $nextComment;
-
-    my $headCommit = headCommit();
-
-    while(my $commitLine=<$history>){
-      chomp($commitLine);
-      if($commitLine =~ m/^(\w+)\s(.*)/){
-       my ($sha, $comment) = ($1,$2);
-       if($sha eq $headCommit){
-          my $nextCommitLine = <$history>;
-          chomp($nextCommitLine);
-          if($nextCommitLine =~ m/^(\w+)\s(.*)/){
-            ($nextSha, $nextComment) = ($1,$2);
-            print "\n\t ⏩ ", $nextComment, "\n\n";
-          }else{
-              die $nextCommitLine
-          }
-          last
-       }
-     }
-    }
-     if($nextSha eq $to) {
-       system "git checkout $branch";
-       exit(0)
-     }elsif($nextSha){
-       debug($nextComment, "==>", $nextSha);
-       system "git checkout $nextSha 2> /dev/null"; 
-     }
-    
-    $history->close();   
+sub initCommits {
+  ("$from 🚀\n", $git->log('--reverse', '--oneline', '--no-abbrev-commit', $branch, "$from...$to"))
 }
 
 sub nextCommit {
-   
+  my $index = shift;
+  if($index == $#commits){
+    end()
+  }else{ 
+    my $commitLine = $commits[$index];
+    if($commitLine =~ /^(\w+)\s(.*)/){
+      my ($sha1, $comment) = ($1, $2);
+      print " ⏩ $comment\n";
+      $git->checkout($sha1);
+      ++$index
+    }
+  }
 }
 
 sub prevCommit {
     
 }
 
+sub end {
+  $git->checkout($branch);
+  exit(0)
+}
+
 sub dryRun {
-  my $branch = $repo->branch();
-  unless($continueSession){
-    die "\n\t 💥 Start commit must be provided with:\n\t\t --from COMMIT-HASH\n" unless  $from;
-  }
+  my $branch = $git->branch();
+
+  die "\n\t 💥 Start commit must be provided with:\n\t\t --from COMMIT-HASH\n" unless  $from;
+
 
   print " 🚀 ", $branch, "\n";
   print "from: $from", "\n";
   print "to: $to", "\n";
-  print "Session file $sessionFile", "\n";
-  print "CMD: git log --reverse --oneline --no-abbrev-commit $branch > $sessionFile", "\n";
-  print "Init history.", "\n";
-  print "CMD: git checkout $from", "\n" unless $continueSession;
-  print "Starting...", "\n";
+
+  print "\n";
+
+  for my $commitLine ( initCommits() ){
+    print "\t", $commitLine, "\n"
+  }
   exit(0)
 }
 
